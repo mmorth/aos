da82485300ccee71c93054a6d75c411e10bc97cd
Austin Schuh <austin.linux@gmail.com>
2025-01-16 00:09:03 -0800

    Update rehost script to push to the bucket
    There's now a bucket behind the site, use that.
    
    Change-Id: Iccc7963aaac75894cf4016781f1bd70675121f0a
    Signed-off-by: Austin Schuh <austin.linux@gmail.com>

--- tools/rehost.sh
+++ tools/rehost.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 #
-# This script makes it easy to rehost dependencies on software.frc971.org so
-# we are self hosted everywhere consistently.
+# Rehosts a dependency to realtimeroboticsgroup.org/build-dependencies
+# There's a gcp bucket behind this all, so just push to it.
+set -xe
 
-# ./rehost.sh github.com/bazelbuild/rules_nodejs/releases/download/4.4.6/rules_nodejs-4.4.6.tar.gz
-set -e
+SOURCE="https://${1}"
+echo "${SOURCE}" gs://austin-vpn-build-dependencies/$1
 
-readonly LOCALPATH="$(basename "${1}")"
+FILE=$(mktemp)
+function cleanup {
+  rm -rf "$FILE"
+}
 
-# Strip off any http/https prefix to make things easy below.
-STRIPPED_URL="${1#"https://"}"
-STRIPPED_URL="${STRIPPED_URL#"http://"}"
+trap cleanup EXIT
+trap cleanup SIGINT
 
-curl -L "https://${STRIPPED_URL}" -o "${LOCALPATH}"
+curl -L "${SOURCE}" -o "${FILE}"
 
-echo "https://realtimeroboticsgroup.org/build-dependencies/${STRIPPED_URL}"
+echo gs://austin-vpn-build-dependencies/$1
 
-readonly TARGET_DIR="/data/files/frc971/Build-Dependencies"
-
-ssh software.frc971.org mkdir --m=775 -p "$(dirname "${TARGET_DIR}/${STRIPPED_URL}")"
-rsync --progress -v --ignore-existing "${LOCALPATH}" software.frc971.org:"${TARGET_DIR}/${STRIPPED_URL}"
-ssh software.frc971.org "find ${TARGET_DIR}/ -type f ! -user www-data ! -group www-data -exec chmod 444 {} \; && find ${TARGET_DIR}/ -type d ! -user www-data ! -group www-data -exec chmod 775 {} \; && find ${TARGET_DIR}/ -type d ! -user www-data ! -group www-data -exec chown www-data:www-data {} \;"
+gsutil cp "${FILE}" "gs://austin-vpn-build-dependencies/$1"
