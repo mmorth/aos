1b3d2eef81be920a3687fb11cb7e1f4cf85c2ede
James Kuszmaul <james.kuszmaul@bluerivertech.com>
2025-02-12 14:12:38 -0800

    VLOG more in memory_estimation.h
    Change-Id: Ic93deae20bcd5bcd21c275fa9510dbc71f879b92
    Signed-off-by: James Kuszmaul <james.kuszmaul@bluerivertech.com>

--- aos/ipc_lib/memory_estimation.cc
+++ aos/ipc_lib/memory_estimation.cc
@@ -1,5 +1,7 @@
 #include "aos/ipc_lib/memory_estimation.h"
 
+#include <map>
+
 #include "aos/ipc_lib/memory_mapped_queue.h"
 namespace aos::ipc_lib {
 size_t TotalSharedMemoryUsage(const aos::Configuration *config,
@@ -8,10 +10,25 @@ size_t TotalSharedMemoryUsage(const aos::Configuration *config,
   const flatbuffers::Vector<flatbuffers::Offset<aos::Channel>> *channels =
       config->channels();
   CHECK(channels != nullptr);
+  // Stores all channels by size, for sorted displays.
+  // Indexed by channel memory usage, with elements that are a displayable name
+  // of the channel.
+  std::multimap<size_t, std::string> channel_sizes;
   for (const aos::Channel *channel : *channels) {
     if (aos::configuration::ChannelIsReadableOnNode(channel, node)) {
       total_size +=
           LocklessQueueMemorySize(MakeQueueConfiguration(config, channel));
+      if (VLOG_IS_ON(1)) {
+        channel_sizes.emplace(
+            LocklessQueueMemorySize(MakeQueueConfiguration(config, channel)),
+            configuration::CleanedChannelToString(channel));
+      }
+    }
+  }
+
+  if (VLOG_IS_ON(1)) {
+    for (const auto &pair : channel_sizes) {
+      LOG(INFO) << "Channel size (bytes): " << pair.first << " " << pair.second;
     }
   }
   return total_size;
